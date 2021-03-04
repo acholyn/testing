fe53f092813d2445b7b5fd5ac1809aa97ad4ea7d
acholyn <72266023+acholyn@users.noreply.github.com>
2021-03-04 09:16:24 -0500

    Update RA.py

--- RA.py
+++ RA.py
@@ -8,7 +8,7 @@ targets1 = list(target_matrix.index)
 
 
 def open_gds(filename):
-    ''' Take filename input and parse geoquery data. Clean the table to remove unneccessary rows'''
+    #Take filename input and parse geoquery data. Clean the table to remove unneccessary rows
     gds = GEOparse.get_GEO(filepath="%s" % filename)
     Table = gds.table
     metadata = gds.columns
@@ -19,7 +19,7 @@ def open_gds(filename):
 
 
 def ge_data(filename):
-    '''Create gene expression data matrix from parsed geoquery data for plsgenomic input'''
+    #Create gene expression data matrix from parsed geoquery data for plsgenomic input
     data = open_gds(filename)
     #identify which genes are targets from htf matrix data
     data = data.loc[data['IDENTIFIER'].isin(targets1)]
@@ -28,6 +28,6 @@ def ge_data(filename):
 
 
 def connec_data(ge_data):
-  '''Create a tf matrix of same dimensions as gene expression data'''
+  #Create a tf matrix of same dimensions as gene expression data
     connec = target_matrix.loc[target_matrix.index.isin(list(ge_data.index))]
     return connec
